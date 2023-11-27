e04c75b3c0a2348f3cc4679c32d77d8cedef2d40
barbolin-semyon <skat.apps@gmail.com>
2023-11-27 20:56:21 +0300

    change compile version from 33 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.protei.barbolinsp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.protei.barbolinsp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
