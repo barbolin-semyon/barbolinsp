88f5c47d474171831a79dacc85a0dfbfb0db53ee
barbolin-semyon <skat.apps@gmail.com>
2024-01-11 00:19:41 +0300

    create NotesUseCase.kt

---
+++ app/src/main/java/ru/protei/barbolinsp/domain/NotesUseCase.kt
@@ -0,0 +1,33 @@
+package ru.protei.barbolinsp.domain
+
+import kotlinx.coroutines.flow.Flow
+
+class NotesUseCase(private val notesRepository: NotesRepository) {
+    suspend fun fillWithInitialNotes(initialNotes: List<Note>) {
+        for (note in initialNotes) {
+            notesRepository.insert(note)
+        }
+    }
+
+    suspend fun save(note: Note, actualNotes: List<Note>) {
+        if (actualNotes.find { it.id == note.id } != null) {
+            notesRepository.update(note)
+        } else {
+            notesRepository.insert(note)
+        }
+    }
+
+    suspend fun notesFlow(isReverseSorted: Boolean = false): Flow<List<Note>> {
+        return if (isReverseSorted) {
+            notesRepository.getAllNotesOfSortedDesc()
+        } else {
+            notesRepository.getAllNotesOfSortedAsc()
+        }
+    }
+
+    suspend fun deleteById(id: Long) {
+        notesRepository.deleteById(id)
+    }
+
+    suspend fun deleteAll() = notesRepository.deleteAll()
+}
