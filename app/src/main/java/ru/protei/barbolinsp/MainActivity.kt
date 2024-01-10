2743df0d180a215e6771f3f2365e6b15bc025faf
barbolin-semyon <skat.apps@gmail.com>
2024-01-11 00:21:14 +0300

    initialize variables (db, dao, repository, useCase and viewModel)

--- app/src/main/java/ru/protei/barbolinsp/MainActivity.kt
+++ app/src/main/java/ru/protei/barbolinsp/MainActivity.kt
@@ -3,20 +3,46 @@ package ru.protei.barbolinsp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.room.Room
+import ru.protei.barbolinsp.data.NotesDatabase
+import ru.protei.barbolinsp.data.NotesRepositoryDB
+import ru.protei.barbolinsp.domain.NotesUseCase
 import ru.protei.barbolinsp.ui.notes.NotesScreen
 import ru.protei.barbolinsp.ui.notes.NotesViewModel
 import ru.protei.barbolinsp.ui.theme.BarbolinspTheme
 
 class MainActivity : ComponentActivity() {
+    private val database: NotesDatabase by lazy {
+        Room.databaseBuilder(
+            this,
+            NotesDatabase::class.java,
+            "notes.db"
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    private val notesRepo by lazy { NotesRepositoryDB(database.notesDao()) }
+    private val notesUseCase by lazy { NotesUseCase(notesRepo) }
+    private val notesViewModelFactory by lazy {
+        viewModelFactory {
+            initializer {
+                NotesViewModel(notesUseCase)
+            }
+        }
+    }
+    val viewModel: NotesViewModel by viewModels { notesViewModelFactory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,9 +51,8 @@ class MainActivity : ComponentActivity() {
             BarbolinspTheme(isThemeDark) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
-                    val viewModel: NotesViewModel = viewModel()
                     NotesScreen(viewModel) {
                         isThemeDark = isThemeDark.not()
                     }
