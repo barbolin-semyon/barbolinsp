0fda45b7c43849997a24dbf284ed053a551bfdf7
barbolin-semyon <skat.apps@gmail.com>
2024-01-11 00:22:51 +0300

    add empty constructor for val id

--- app/src/main/java/ru/protei/barbolinsp/domain/Note.kt
+++ app/src/main/java/ru/protei/barbolinsp/domain/Note.kt
@@ -5,8 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Note(
-    @PrimaryKey(autoGenerate = true)
-    val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val title: String,
     val text: String,
 )
