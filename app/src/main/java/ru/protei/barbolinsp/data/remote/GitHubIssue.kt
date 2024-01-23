7fe0b4276c59b9514ff41a0f471c86529fe81134
4118BarbolinS.P <skat.apps@gmail.com>
2024-01-23 23:19:17 +0300

    add updateAt

--- app/src/main/java/ru/protei/barbolinsp/data/remote/GitHubIssue.kt
+++ app/src/main/java/ru/protei/barbolinsp/data/remote/GitHubIssue.kt
@@ -1,14 +1,20 @@
 package ru.protei.barbolinsp.data.remote
 
+import com.google.gson.annotations.SerializedName
+import java.util.Date
+
 data class GitHubIssue(
     val number: Long,
     val title: String,
-    val body: String,
+    val body: String?,
+    @SerializedName("updated_at")
+    val updatedAt: Date,
     val state: String?
 ) {
     constructor(
         number: Long,
         title: String,
         body: String,
-    ) : this(number, title, body, null)
+        updatedAt: Date
+    ) : this(number, title, body, updatedAt, null)
 }
--- app/src/main/java/ru/protei/barbolinsp/domain/Note.kt
+++ app/src/main/java/ru/protei/barbolinsp/domain/Note.kt
@@ -2,10 +2,13 @@ package ru.protei.barbolinsp.domain
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.Date
 
 @Entity
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @PrimaryKey (autoGenerate = true)
+    val id: Long = 0,
     val title: String,
     val text: String,
+    val updateAt: Date?,
 )
